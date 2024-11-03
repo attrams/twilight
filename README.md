6d9c8e432a375dfb160d4b4da9dff9d7f73a10d8
attrams <48452451+Attrams@users.noreply.github.com>
2024-11-03 03:47:15 +0000

    add menu section

--- README.md
+++ README.md
@@ -13,6 +13,20 @@
     <h3>Kempinski - Accra</h3>
     <p>
         <a href="https://www.instagram.com/kempinskiaccra/p/C5BblCRIVED/?img_index=2" target="_blank">Afternoon Tea</a>
+    </p>    
+    <h3>Santoku - Accra</h3>
+    <p>
+        <a href="https://www.instagram.com/santokuaccra/p/C_-lPvMieww/?hl=en&img_index=1" target="_blank">Meko noodles</a>
+    </p>
+    <h3>Pomona - Accra</h3>
+    <p>
+        <a href="https://www.instagram.com/pomona.gh/p/DBjnBx0Cbaq/?hl=en" target="_blank">Orange Cocktail</a>
+    </p> 
+    <p>
+        <a href="https://www.instagram.com/pomona.gh/p/DAbgvbBCzx7/?hl=en" target="_blank">Lemon Cocktail</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/pomona.gh/p/C8fKFPvCBAl/?hl=en" target="_blank">Special Cocktail</a>
     </p>
     <h3>Shogun - Accra</h3>
     <p>
@@ -20,6 +34,25 @@
     </p>
     <p>
         <a href="https://www.instagram.com/shogunghana/p/CyVt7tOOjBa/?hl=en" target="_blank">Cocktail</a>
+    </p>
+        <h3>Buka Restaurant - Accra</h3>
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/C9excmwCf4h/?hl=en&img_index=1" target="_blank">Tatale and Aboboi</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/C4uqlYaiRpO/?hl=en" target="_blank">Angwamo (Braised Rice)</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/ClYYT93DmFj/?hl=en" target="_blank">Tuo Zaafi</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/C8eY_NbCxyc/?hl=en" target="_blank">Goat Jollof</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/CbF9c51MhS6/?hl=en" target="_blank">Grilled guinea fowl with crispy fried yam</a>
+    </p>    
+    <p>
+        <a href="https://www.instagram.com/thebukarestaurant/p/CahDx23sp8G/?hl=en" target="_blank">kontomire stew and plantain</a>
     </p>
     <h3>Unsplash</h3>
     <p>
--- js/script.js
+++ js/script.js
@@ -107,3 +107,89 @@ heroSliderPrevBtn.addEventListener('click', slidePrev);
 
 // auto slide
 slideInterval = setInterval(slideNext, 15000);
+
+// view all menu button
+const viewAllMenu = document.getElementById('view-all-menu');
+const menuList = document.querySelector('section#menu .grid-list');
+
+const additionalItems = [
+  {
+    title: 'Orange Cocktail',
+    price: 'GH₵90.00',
+    description:
+      'Fresh Orange Juice: 3 oz, Vodka: 2 oz, Triple Sec: 1 oz, Simple Syrup: 0.5 oz (optional), Club Soda: 2 oz, Ice Cubes: for shaking, Orange Slices: for garnish, Fresh Mint: optional, for garnish',
+    image: './images/drink-1.jpg',
+    label: '',
+  },
+  {
+    title: 'Lemon Cocktail',
+    price: 'GH₵95.00',
+    description:
+      'Lemon Juice: 2 oz, Simple Syrup: 1 oz, Gin or Vodka: 2 oz, Club Soda: 2 oz, Ice Cubes: for shaking, Lemon Slices: for garnish, Fresh Mint: optional, for garnish.',
+    image: './images/drink-2.jpg',
+    label: '',
+  },
+  {
+    title: 'Special Cocktail',
+    price: 'GH₵125.00',
+    description:
+      'White Rum: 2 oz, Coconut Cream: 1 oz, Fresh Pineapple Juice: 2 oz, Crushed Ice: 1 cup, Pineapple Slice: for garnish, Maraschino Cherry: for garnish.',
+    image: './images/drink-3.jpg',
+    label: '',
+  },
+  {
+    title: 'Stir Fry',
+    price: 'GH₵225.00',
+    description:
+      'Meko Noodles, Chicken, Bell Peppers, Carrots, Green Onions, Soy Sauce, Sesame Oil, Garlic, Ginger, Chili Sauce, Sesame Seeds',
+    image: './images/menu-7.jpg',
+    label: 'New',
+  },
+];
+
+let showMore = false;
+
+viewAllMenu.addEventListener('click', function (e) {
+  e.preventDefault();
+  showMore = !showMore;
+
+  if (showMore) {
+    additionalItems.forEach((item) => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <div class="menu-card hover:card">
+          <figure class="card-banner img-holder" style="--width: 100; --height: 100;">
+            <img src="${item.image}" alt="${
+        item.title
+      }" loading="lazy" width="100" height="100" class="img-cover" onerror="this.style.display='none'">
+          </figure>
+          <div>
+            <div class="title-wrapper">
+              <h3 class="title-3">
+                <a href="#" class="card-title">${item.title}</a>
+              </h3>
+              ${
+                item.label
+                  ? `<span class="badge label-1">${item.label}</span>`
+                  : ''
+              }
+              <span class="span title-2">${item.price}</span>
+            </div>
+            <p class="card-text label-1">${item.description}</p>
+          </div>
+        </div>
+      `;
+      menuList.appendChild(li);
+    });
+
+    viewAllMenu.querySelectorAll('.text').forEach((element) => {
+      element.textContent = 'View Less';
+    });
+  } else {
+    const removeExtraMenuItems = menuList.querySelectorAll('li:nth-child(n+7)');
+    removeExtraMenuItems.forEach((item) => item.remove());
+    viewAllMenu.querySelectorAll('.text').forEach((element) => {
+      element.textContent = 'View All Menu';
+    });
+  }
+});
